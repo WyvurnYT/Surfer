7dc4044f3785dc3ca919f8b155dbeee3c0397dbd
WyvurnYT <153701920+WyvurnYT@users.noreply.github.com>
2025-05-26 22:48:27 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -71,8 +71,8 @@ div[title="Click to open AB cloaked. Ctrl+click to open full url."] {
 (function() {
   function updateMsg() {
     var el = document.querySelector(".rhnewtab-msg-40821");
-if (el && el.innerText !== "Surfer Browser\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\nDue to limitations of the browser, some links may not work.") {
-  el.innerText = "Surfer Browser\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\nDue to limitations of the browser, some links may not work.";
+if (el && el.innerText !== "Surfer Browser\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\nDue to limitations of the browser, some links may not work.") {
+  el.innerText = "Surfer Browser\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\nDue to limitations of the browser, some links may not work.";
 }
   }
   // Initial check
